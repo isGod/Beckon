dd08ad5995cfc26a859f3c2e4dbe808f949ede56
1661225999qq.com <zL@hongfengwangluodeiMac.local>
2016-10-19 09:51:44 +0800

    创建工程

---
+++ Beckon/Beckon/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Beckon
+//
+//  Created by zL on 2016/10/19.
+//  Copyright © 2016年 zL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ Beckon/Beckon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Beckon
+//
+//  Created by zL on 2016/10/19.
+//  Copyright © 2016年 zL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
